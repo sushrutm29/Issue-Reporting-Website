cb7e344da3b12f8c424080cb2c78625272d0790a
lun-weichang <lunweichang539@gmail.com>
2020-04-02 13:22:28 -0400

    Setup initial folders/files and installed necessary packaged

---
+++ issue-reporting-site/src/components/editPassword.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+class editPassword extends Component {
+	render() {
+        let html_body = (
+            <div>
+                <Switch>
+                    {/* <Route path="" exact component={} /> */}
+                </Switch>
+            </div> 
+        );
+        return html_body;
+    }
+}
+
+export default editPassword;
