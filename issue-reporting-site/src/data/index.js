817b0dacb9cbe79fc6c5ded8f120ad2f93d7ed79
lun-weichang <lunweichang539@gmail.com>
2020-04-06 20:17:43 -0400

    Added configuration files and data files

---
+++ issue-reporting-site/src/data/index.js
@@ -0,0 +1,9 @@
+const users = require('./users');
+const departments = require('./dept');
+const post = require('./posts');
+
+module.exports = {
+    users: users,
+    departments: dept,
+    posts: posts
+};
