5b98daacdb5dd706664480a84d349ec27b0b0398
lun-weichang <lunweichang539@gmail.com>
2020-04-30 13:51:39 -0400

    fixed a few errors so the server side codes may run

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const configRoutes = require("./routes");
-app.use(express.json())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 configRoutes(app);
 app.listen(3000, () => {
